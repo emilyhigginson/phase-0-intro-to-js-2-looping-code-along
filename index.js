2c12c91733c596f5fb46c79e2747d56b6be1ab4e
Emily Higginson <emilyhigginson14@gmail.com>
2021-12-02 10:22:58 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const name = ['Lisa', 'Kaitlin', 'Jan'];
+const event = 'surprise';
+
+function writeCards(names, event) {
+    let newArr = [];
+    for (let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        debugger;
+    }
+    return newArr;
+}
+function countDown (int) {
+    let i=10;
+    while (i >= 0) {
+        console.log(i);
+        i--
+    }
+    return int;
+}
